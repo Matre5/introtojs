5def29bd8d45aece3429258f99d690e90843fbf1
Matre Aiguokhian <105281356+Matre5@users.noreply.github.com>
2022-06-24 01:54:51 -0700

    initialcommit

---
+++ index.js
@@ -0,0 +1,7 @@
+alert(
+    "My name is Aiguokhian Matre \n I am 185cm tall \n I am from Nigeria"
+)
+function Clickme() {
+     document.getElementById('demo').innerHTML="My name is Matre Aiguokhian. I am 185cm tall. I am from Nigeria"
+     
+}
